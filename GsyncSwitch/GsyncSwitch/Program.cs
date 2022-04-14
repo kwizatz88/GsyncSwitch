188e051a69524eac0e7e691c3ee02368eda37309
kwizatz88 <71530061+kwizatz88@users.noreply.github.com>
2022-04-14 11:26:28 +0200

    Add files via upload

---
+++ GsyncSwitch/GsyncSwitch/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Drawing;
+using Microsoft.Win32;
+
+namespace GsyncSwitch
+{
+    class ControlContainer : IContainer
+    {
+        ComponentCollection _components;
+
+        public ControlContainer()
+        {
+            _components = new ComponentCollection(new IComponent[] { });
+        }
+
+        public void Add(IComponent component) { }
+        public void Add(IComponent component, string Name) { }
+        public void Remove(IComponent component) { }
+
+        public ComponentCollection Components
+        {
+            get { return _components; }
+        }
+
+        public void Dispose()
+        {
+            _components = null;
+        }
+    }
+
+    static class Program
+    {
+        
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            IconClass sc = new IconClass();
+//            sc.notifyIcon1.ShowBalloonTip(1000);
+
+            Application.Run();
+        }
+    }
+
+    class IconClass
+    {
+        ControlContainer container = new ControlContainer();
+        public NotifyIcon notifyIcon1;
+        private ContextMenuStrip contextMenu;
+        private ToolStripMenuItem switchGsync;
+        private ToolStripMenuItem exitApplication;
+        private ToolStripMenuItem launchAtStartup;
+
+        // The path to the key where Windows looks for startup applications
+        public RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+        public IconClass()
+        {
+            this.notifyIcon1 = new NotifyIcon(container);
+            this.notifyIcon1.Icon = new Icon(this.GetType(), "Letter_G.ico");
+            this.notifyIcon1.Text = "Gsync Switch by KwizatZ";
+
+            this.notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipTitle = "Gsync Switch";
+            this.notifyIcon1.BalloonTipText = "made by KwizatZ";
+
+            this.notifyIcon1.Visible = true;
+
+            contextMenu = new ContextMenuStrip();
+            switchGsync = new ToolStripMenuItem();
+            exitApplication = new ToolStripMenuItem();
+            launchAtStartup = new ToolStripMenuItem();
+
+            this.notifyIcon1.ContextMenuStrip = contextMenu;
+
+            this.notifyIcon1.DoubleClick += NotifyIcon_DoubleClick;
+
+            switchGsync.Text = "Switch Gsync";
+            switchGsync.Click += new EventHandler(SwitchGsync_Click);
+            contextMenu.Items.Add(switchGsync);
+
+            exitApplication.Text = "Exit..";
+            exitApplication.Click += new EventHandler(ExitApplication_Click);
+            contextMenu.Items.Add(exitApplication);
+
+            launchAtStartup.Text = "Launch at Windows startup";
+            // Check to see the current state (running at startup or not)
+            if (rkApp.GetValue("GsyncSwitch") == null)
+            {
+                // The value doesn't exist, the application is not set to run at startup
+                launchAtStartup.Checked = false;
+            }
+            else
+            {
+                // The value exists, the application is set to run at startup
+                launchAtStartup.Checked = true;
+            }
+            launchAtStartup.Click += new EventHandler(LaunchAtStartup_Click);
+            contextMenu.Items.Add(launchAtStartup);
+
+        }
+
+        private void LaunchAtStartup_Click(object sender, EventArgs e)
+        {
+            if (!launchAtStartup.Checked)
+            {
+                // Add the value in the registry so that the application runs at startup
+                rkApp.SetValue("GsyncSwitch", Application.ExecutablePath);
+                launchAtStartup.Checked = true;
+            }
+            else
+            {
+                // Remove the value from the registry so that the application doesn't start
+                rkApp.DeleteValue("GsyncSwitch", false);
+                launchAtStartup.Checked = false;
+            }
+        }
+
+        private void ExitApplication_Click(object sender, EventArgs e)
+        {
+            this.notifyIcon1.Visible = false;
+
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void SwitchGsync(object sender, EventArgs e)
+        {
+            Process gsyncSwitchEXE = new Process();
+            gsyncSwitchEXE.StartInfo.FileName = "GsyncSwitchEXE.exe";
+            //            gsyncSwitchEXE.StartInfo.Arguments = "DemoText";
+            gsyncSwitchEXE.Start();
+        }
+
+        private void SwitchGsync_Click(object sender, EventArgs e)
+        {
+            SwitchGsync(sender,e);
+        }
+
+        private void NotifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            SwitchGsync(sender, e);
+        }
+    }
+
+}
